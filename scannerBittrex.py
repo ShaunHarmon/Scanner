7c5eec4faa8488be2a02d164e74941c94265fd11
ShaunHarmon <shaun.harmon@gmail.com>
2018-01-10 21:09:21 -0600

    update

--- scannerBittrex.py
+++ scannerBittrex.py
@@ -107,7 +107,7 @@ def main():
     updateCoinList()
 
 if __name__ == "__main__":
-   # main()
+    
     port = int(os.getenv('PORT', 5000))
     app.run(debug=False, host='0.0.0.0',port=port)
-    
+    main()
