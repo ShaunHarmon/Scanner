06e9ae504ad3b4212493d46b584e9844db4245a5
ShaunHarmon <shaun.harmon@gmail.com>
2018-01-10 20:23:10 -0600

    update

--- scannerBittrex.py
+++ scannerBittrex.py
@@ -108,5 +108,5 @@ def main():
 
 if __name__ == "__main__":
     port = int(os.environ('PORT', 5000))
-    app.run(debug=TRUE, host='0.0.0.0',port=port)
+    app.run(debug=False, host='0.0.0.0',port=port)
     main()
